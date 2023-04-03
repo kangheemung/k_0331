a95bb7c91449b11070702dec963cc9c2508497e6
kangheemung <kurotamashi30@gmail.com>
2023-04-03 13:36:51 +0000

    useredit

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,9 +6,14 @@ class SessionsController < ApplicationController
     if user&&user.authenticate(params[:users][:password])
       session[:user_id] = user.id
       flash[:notice]="ログインしました。"
-      redirect_to root_path
+      redirect_to users_show_path(user.id)
     else
       :new
     end
+  end
+  def destroy
+  session[:user_id]=nil
+  flash[:notice] ="logout"
+  redirect_to root_path
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,17 @@
 Rails.application.routes.draw do
 
+  get 'posts/index'
+  get 'posts/new'
+  get 'posts/show'
+  get 'posts/edit'
   root 'users#index'
   get 'users/new'=>'users#new', as:'users_new'
   post'users/create'=>'users#create'
   get 'users/edit'=>'users#edit',as:'users_edit'
+  delete 'users/destoy'=>'users#destroy',as: 'users_destroy'
   get 'users/:id'=>'users#show',as:'users_show'
-  get 'sessions/new'=>'sessions#new', as:'sessions_new' 
+  get 'sessions/new'=>'sessions#new', as:'sessions_new'
+  delete'sessions/destoy'=>'sessions#destroy',as: 'sessions_destroy'
   post 'sessions/create'=>'sessions#create', as:'sessions_create'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
