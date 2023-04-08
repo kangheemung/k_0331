be0abc2b6bb1c238d87f9e9b568a29052dc153ee
kangheemung <kurotamashi30@gmail.com>
2023-04-08 15:32:22 +0000

    recommit

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,8 @@
 class User < ApplicationRecord
     has_secure_password
       has_many :posts
-     #validates :password, length: { maximum: 140 }
+      validates :email, presence: true, length: { maximum: 255 }
+     validates :username, presence: true, length: { maximum: 50 }
+     validates :password, length: { maximum: 140 }
+     validates :email, presence: true
 end
