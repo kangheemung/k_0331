1106d5e0a2c441486f01f2531a4d94744a03d256
kangheemung <kurotamashi30@gmail.com>
2023-04-09 15:08:49 +0000

    users_signup_test

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,41 +1,36 @@
 class UsersController < ApplicationController
   def index
       @users=User.all
- 
   end
 
   def new
     @user=User.new
-    p"==============="
-    p params
-    p"==================new"
-   
+    #p"==============="
+    #p params
+    #p"==================new"
   end
+  
   def create
-   @user=User.new(user_params)
-    p"========create======"
-    p params
-    p"=================="
-    p"============"
-      p @user.errors.full_messages
-      p"============"
-    user=User.new(user_params)
-    if user.save
-      p"============"
-      p @user.errors.full_messages
-      p"============"
-      session[:user_id] = user.id
-      redirect_to　users_show_path(user.id)
-      
-    else
-      render :new
-    end
+    @user=User.new(user_params)
+      if @user.save
+        flash[:success] = "Welcome to the Sample App!"
+        session[:user_id] = @user.id
+        p"========create======="
+        p params
+        p"=================="
+        #p"============"
+        #p @user.errors.full_messages
+        #p"============"
+      redirect_to users_show_path(@user.id)
+      else
+        render :new
+      end
   end
-
+  
   def show
     @user=User.find_by(id: params[:id])
-   
   end
+  
   def update
      #p"============"
         #   p @user.errors.full_messages
@@ -49,6 +44,7 @@ class UsersController < ApplicationController
         render :edit
       end
   end
+  
   def edit
       @user=User.find_by(id: params[:id])
   end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,13 +2,12 @@ class User < ApplicationRecord
     has_secure_password
      has_many :posts
      before_save { email.downcase! }
+     validates :username, presence: true, length: { maximum: 50 }
+     VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
      validates :email, presence: true, length: { maximum: 255 },
                  format: { with: VALID_EMAIL_REGEX },
                       uniqueness: { case_sensitive: false }
-                  
-     VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-     validates :username, presence: true, length: { maximum: 50 }
       validates :password, presence: true, length: { minimum: 6 }
-
+      
     
 end
