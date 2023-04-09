a81aa4442de71f999d0e9ac59bcc464483b752fb
kangheemung <kurotamashi30@gmail.com>
2023-04-09 09:04:42 +0000

    test

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,14 @@
 class User < ApplicationRecord
     has_secure_password
-      has_many :posts
-      validates :email, presence: true, length: { maximum: 255 }
+     has_many :posts
+     before_save { email.downcase! }
+     validates :email, presence: true, length: { maximum: 255 },
+                 format: { with: VALID_EMAIL_REGEX },
+                      uniqueness: { case_sensitive: false }
+                  
+     VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
      validates :username, presence: true, length: { maximum: 50 }
-     validates :password, length: { maximum: 140 }
-     validates :email, presence: true
+      validates :password, presence: true, length: { minimum: 6 }
+
+    
 end
--- db/schema.rb
+++ db/schema.rb
@@ -15,8 +15,6 @@ ActiveRecord::Schema.define(version: 2023_04_03_124148) do
   create_table "posts", force: :cascade do |t|
     t.text "content"
     t.integer "user_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
